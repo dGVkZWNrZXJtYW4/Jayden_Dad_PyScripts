af59a567d88c7bd97cce3178064da0b6821a6fe5
dGVkZWNrZXJtYW4 <149637555+dGVkZWNrZXJtYW4@users.noreply.github.com>
2023-11-23 16:54:40 -0800

    Update square_draw.py

--- square_draw.py
+++ square_draw.py
@@ -4,7 +4,10 @@ import turtle
 screen = turtle.Screen()
 my_turtle = turtle.Turtle()
 
-# Your code goes here
+# Draw a square
+for _ in range(4):
+    my_turtle.forward(100)  # Move the turtle forward by 100 units
+    my_turtle.right(90)     # Turn the turtle right by 90 degrees
 
 # Close the turtle graphics window when clicked
 screen.exitonclick()
