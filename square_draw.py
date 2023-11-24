656f09bfd82b60d0a8e65df48a1e7779b38eabbd
dGVkZWNrZXJtYW4 <149637555+dGVkZWNrZXJtYW4@users.noreply.github.com>
2023-11-23 16:54:10 -0800

    Create square_draw.py

---
+++ square_draw.py
@@ -0,0 +1,10 @@
+import turtle
+
+# Create a turtle screen and a turtle
+screen = turtle.Screen()
+my_turtle = turtle.Turtle()
+
+# Your code goes here
+
+# Close the turtle graphics window when clicked
+screen.exitonclick()
